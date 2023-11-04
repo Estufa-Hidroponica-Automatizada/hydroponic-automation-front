8311d223d72edb8e98d76965f06279797b7345d0
Silas Lima <silaslima@usp.br>
2023-11-04 11:33:41 -0300

    feat: download photo

--- src/components/actions-bar/types.ts
+++ src/components/actions-bar/types.ts
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 
 export interface ActionsBarProps {
   buttons: [ActionButton, ActionButton];
+  notPadding?: boolean;
 }
 
 interface ActionButton {
--- src/pages/photo/photo.page.tsx
+++ src/pages/photo/photo.page.tsx
@@ -1,17 +1,18 @@
-import { Button, Skeleton } from "antd";
+import { Skeleton } from "antd";
+import { ActionsBar, ResponsiveActionsContainer } from "components";
+import dayjs from "dayjs";
 import { usePhoto } from "hooks";
 import { useEffect, useRef } from "react";
 
 export const PhotoPage = () => {
   const initialRender = useRef(true);
-
   const { getPhoto, isLoading, photo } = usePhoto();
 
-  useEffect(() => {
-    const getCurrentPhoto = async () => {
-      await getPhoto();
-    };
+  const getCurrentPhoto = async () => {
+    await getPhoto();
+  };
 
+  useEffect(() => {
     if (initialRender.current) {
       initialRender.current = false;
       getCurrentPhoto();
@@ -20,26 +21,45 @@ export const PhotoPage = () => {
 
   const isPortrait = window.innerHeight > window.innerWidth;
 
+  // TO DO: adjust size
   const height = isPortrait
     ? `${window.innerWidth / 1.33}px`
-    : `${0.6 * window.innerHeight}px`;
+    : `${0.75 * window.innerHeight}px`;
 
   const width = isPortrait
     ? `${window.innerWidth}px`
-    : `${1.33 * (0.6 * window.innerHeight)}px`;
+    : `${1.33 * (0.75 * window.innerHeight)}px`;
 
   const photoStyle = { width, height };
 
+  const handleDownload = () => {
+    const a = document.createElement("a");
+    a.href = photo ?? "";
+    a.download = `${dayjs().format("DD-MM-YYYY-hhmm")}.jpeg`;
+    a.click();
+  };
+
   return (
-    <div className="d-flex justify-content-center h-100">
+    <div className="d-flex flex-column justify-content-center gap-3 h-100">
+      <ResponsiveActionsContainer>
+        <ActionsBar
+          buttons={[
+            {
+              text: "Recarregar imagem",
+              handleClick: getCurrentPhoto,
+              loading: isLoading,
+            },
+            { text: "Baixar imagem", handleClick: handleDownload },
+          ]}
+          notPadding
+        />
+      </ResponsiveActionsContainer>
       {isLoading ? (
-        <Skeleton.Image active style={photoStyle} />
+        <Skeleton.Image active style={{ width: "100%" }} />
       ) : (
         photo && (
           <div className="d-flex flex-column gap-3">
-            <Button type="primary">Baixar imagem</Button>
-
-            <img alt="img" src={photo} style={photoStyle} />
+            <img alt="img" src={photo} />
           </div>
         )
       )}
